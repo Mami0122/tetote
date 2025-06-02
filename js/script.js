ae4e9f4a7a015bb61236d147c4cb45d4f2813d45
Mami Hattori <humor_thx@icloud.com>
2025-06-02 17:47:12 +0900

    フォーム入力時の入力チェック処理がdebounceされるように修正

--- js/script.js
+++ js/script.js
@@ -355,10 +355,28 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
 
-    [...mergedRequiredInputs, acceptance].forEach((input) => {
-      input.addEventListener('input', checkInputs);
-    });
+    const debounce = (mainFunction, delay) => {
+      // Declare a variable called 'timer' to store the timer ID
+      let timer;
 
+      // Return an anonymous function that takes in any number of arguments
+      return function (...args) {
+        // Clear the previous timer to prevent the execution of 'mainFunction'
+        clearTimeout(timer);
+
+        // Set a new timer that will execute 'mainFunction' after the specified delay
+        timer = setTimeout(() => {
+          mainFunction(...args);
+        }, delay);
+        console.log('debounce処理');
+      };
+    };
+
+    const debouncedCheckInputs = debounce(checkInputs, 300);
+
+    [...mergedRequiredInputs, acceptance].forEach((input) => {
+      input.addEventListener('input', debouncedCheckInputs);
+    });
 
     checkInputs();
   }
